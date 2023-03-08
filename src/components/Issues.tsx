ff0a79707d102710089655239b454360b350bb85
anama <anama@DESKTOP-NC3SKCA.(none)>
2023-03-08 13:01:23 +0200

    added RadixUI

--- src/components/Issues.tsx
+++ src/components/Issues.tsx
@@ -1,9 +1,12 @@
+import React from "react";
 import { IssuesArray } from "../interface/interfaces";
 import Moment from "react-moment";
 import { BsTrash3Fill, BsHourglassSplit } from "react-icons/bs";
-import { HiOutlineDesktopComputer } from "react-icons/hi";
 import { FiAlertOctagon } from "react-icons/fi";
 import { CgTimelapse } from "react-icons/cg";
+import * as Accordion from "@radix-ui/react-accordion";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+
 interface Props {
   issueArray: IssuesArray[];
   setIssueArray: React.Dispatch<React.SetStateAction<IssuesArray[]>>;
@@ -21,6 +24,42 @@ const Issues = ({ issueArray, setIssueArray }: Props) => {
       )
     );
   };
+
+  const AccordionTrigger = React.forwardRef(
+    (
+      { children, ...props }: { children: string },
+      forwardedRef: React.Ref<HTMLButtonElement>
+    ) => (
+      <Accordion.Header>
+        <Accordion.Trigger
+          {...props}
+          ref={forwardedRef}
+          className="inline-flex"
+        >
+          {children}
+          {children.length > 30 ? (
+            <ChevronDownIcon
+              className="bg-yellow-400 font-medium ml-2 h-3 w-3 mt-1 block"
+              aria-hidden
+            />
+          ) : (
+            ""
+          )}
+        </Accordion.Trigger>
+      </Accordion.Header>
+    )
+  );
+
+  const AccordionContent = React.forwardRef(
+    (
+      { children, ...props }: { children: string },
+      forwardedRef: React.Ref<HTMLDivElement>
+    ) => (
+      <Accordion.Content {...props} ref={forwardedRef}>
+        <div>{children}</div>
+      </Accordion.Content>
+    )
+  );
 
   return (
     <div className="flex flex-col bg-zinc-50">
@@ -61,9 +100,23 @@ const Issues = ({ issueArray, setIssueArray }: Props) => {
                       <td className="whitespace-nowrap px-2 py-4 text-zinc-600">
                         {issue.login}
                       </td>
-                      <td className="max-w-lg whitespace-wrap px-8 py-4">
-                        {issue.issue.substring(0, 30)}{" "}
-                        {issue.issue.length > 30 ? "..." : ""}
+                      <td className="block max-w-lg whitespace-wrap px-8 py-4">
+                        <Accordion.Root
+                          type="single"
+                          defaultValue="item-1"
+                          collapsible
+                        >
+                          <Accordion.Item value="item-1">
+                            <AccordionTrigger>
+                              {issue.issue.substring(0, 60)}
+                            </AccordionTrigger>
+                            {issue.issue.length > 30 ? (
+                              <AccordionContent>{issue.issue}</AccordionContent>
+                            ) : (
+                              ""
+                            )}
+                          </Accordion.Item>
+                        </Accordion.Root>
                       </td>
                       <td className="whitespace-nowrap px-2 py-4 text-zinc-600">
                         <Moment fromNow className="font-light">
@@ -78,20 +131,22 @@ const Issues = ({ issueArray, setIssueArray }: Props) => {
                         }
                       >
                         {issue.isFixed ? (
-                          <CgTimelapse className="text-yellow-400 text-lg ml-4  w-5 h-5" />
+                          <CgTimelapse className="text-yellow-400 text-lg ml-4 w-5 h-5" />
                         ) : (
                           <FiAlertOctagon className="text-red-400 text-lg ml-4  w-5 h-5" />
                         )}
                       </td>
-                      <td className="whitespace-wrap px-1 py-4 flex gap-x-4">
-                        <BsHourglassSplit
-                          onClick={() => handleStatus(issue.id)}
-                          className="text-yellow-400 text-lg group/edit invisible group-hover/item:visible"
-                        />
-                        <BsTrash3Fill
-                          onClick={() => handleDelete(issue.id)}
-                          className="text-red-400 text-lg group/edit invisible group-hover/item:visible"
-                        />
+                      <td className="whitespace-wrap px-3 py-4 ">
+                        <div className="flex gap-x-4">
+                          <BsHourglassSplit
+                            onClick={() => handleStatus(issue.id)}
+                            className="text-yellow-400 text-lg group/edit invisible group-hover/item:visible"
+                          />
+                          <BsTrash3Fill
+                            onClick={() => handleDelete(issue.id)}
+                            className="text-red-400 text-lg group/edit invisible group-hover/item:visible"
+                          />
+                        </div>
                       </td>
                     </tr>
                   </tbody>
